80ae7b82fe9e68ec04571d5236f714bec8e937c6
K9090 <K9090@student.jamk.fi>
2017-01-11 15:15:00 +0200

    Lottokone added

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -6,29 +6,56 @@ namespace JAMK.IT
 {
   class BlackJack
   {
-    static void Main()
-    {
-      int myNumber = 17;
-      int theirNumber;
-      System.Console.WriteLine("*** BlackJack! ***");
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
-      {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
+        static void Main()
         {
-          System.Console.WriteLine("You win.");
+            MBlackjack();
+            Veikkaus.Lotto.DrawLotto();
         }
-        else {
-          System.Console.WriteLine("You lose.");
-        }
-      }
+        static void MBlackjack()
+        { 
+      int myNumber;
+      int theirNumber= 0;
+      string exit = "null";
+
+                Random rnd = new Random();
+                myNumber = rnd.Next(10, 22);
+                Console.WriteLine("*** BlackJack! ***");
+
+            do {
+                Console.Write("Can you beat my number? Enter any number between 1-21: ");
+                
+                //reading and converting
+                theirNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                //comparing that given number is valid
+                while (theirNumber < 1 || theirNumber > 21)
+                {
+                    Console.Write("The given number is out of limits! From between the numbers 1-21, try again: ");
+                    theirNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                               
+                //comparing
+                if (theirNumber >= myNumber && theirNumber <= 21)
+                {
+                        Console.WriteLine("My number: " + myNumber + ". You win.");
+                        Console.WriteLine();
+                }
+                else
+                {
+                        Console.WriteLine("My number: " + myNumber + ". You lose.");
+                        Console.WriteLine();
+                }
+                Console.WriteLine("Play again? Press any number to replay. Type 'x' or 'exit' to exit.");
+                
+                exit = Console.ReadLine();
+               /* if (exit != "x" && exit != "exit")
+                {
+                    if (theirNumber == )
+                    Console.WriteLine("Plese, do not deviate from the given options!");
+                }
+                */
+
+             } while (exit != "x" && exit != "exit");
     }
   }
 }
